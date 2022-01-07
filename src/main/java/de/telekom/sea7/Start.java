d67a97564e2069bf9317eadae3de8e57062f779d
Oliver Reiger <oliver.reiger@mac.com>
2022-01-07 09:15:31 +0100

    Grundprojekt

---
+++ src/main/java/de/telekom/sea7/Start.java
@@ -0,0 +1,11 @@
+package de.telekom.sea7;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dies ist die Startklasse meines zweiten Projekts.");
+
+	}
+
+}
---
+++ src/main/java/de/telekom/sea7/sqrFunction.java
@@ -0,0 +1,9 @@
+package de.telekom.sea7;
+
+public class sqrFunction {
+	
+	public static int sqr(int operand) {
+		return operand * operand;
+	}
+
+}
